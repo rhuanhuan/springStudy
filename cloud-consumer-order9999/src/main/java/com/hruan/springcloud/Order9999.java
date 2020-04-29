588ec3f1c3376d5130662e50565b441c4c57b695
rhuanhuan <1210098674@qq.com>
2020-04-29 23:48:24 +0800

    添加order服务作为payment下游服务

---
+++ cloud-consumer-order9999/src/main/java/com/hruan/springcloud/Order9999.java
@@ -0,0 +1,11 @@
+package com.hruan.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Order9999 {
+    public static void main(String[] args) {
+        SpringApplication.run(Order9999.class, args);
+    }
+}

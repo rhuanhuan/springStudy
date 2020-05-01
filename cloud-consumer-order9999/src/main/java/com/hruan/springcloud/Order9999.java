4f95f629b7c034ae736a490ee87f45334aaa4120
rhuanhuan <1210098674@qq.com>
2020-05-01 16:17:41 +0800

    注册order9999到eureka server

--- cloud-consumer-order9999/src/main/java/com/hruan/springcloud/Order9999.java
+++ cloud-consumer-order9999/src/main/java/com/hruan/springcloud/Order9999.java
@@ -2,8 +2,10 @@ package com.hruan.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Order9999 {
     public static void main(String[] args) {
         SpringApplication.run(Order9999.class, args);

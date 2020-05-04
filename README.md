c4e3c1a3cfda06fbcbf26711e97a15f60f879f85
rhuanhuan <1210098674@qq.com>
2020-05-04 10:55:57 +0800

    rest template添加使用response entity的写法

--- README.md
+++ README.md
@@ -1,15 +1,29 @@
 这是一个跟着B站视频学习Spring Cloud的代码库。
 
-1. 在maven父工程下面，启动payment和order两个子工程服务，并添加热部署;
-2. 将两个服务中公共的部分抽成common模块，别的服务引用他;
+1. 在maven父工程下面，启动payment和order两个子工程服务，并添加**热部署**;
+2. 将两个服务中**公共的部分抽成common模块**，别的服务引用他;
 
-Eureka注册中心篇:
+[Eureka](https://github.com/Netflix/eureka)注册中心篇:
 1. 添加服务注册中心Eureka集群; 保留自我保护机制
 2. 添加payment作为集群，并注册到Eureka上; 
     - 添加服务id信息
     - enable discovery client
 
-Zookeeper & Consul 使用起来配置都十分类似，不过需要外部启动对应的服务。
+[Zookeeper](https://github.com/apache/zookeeper) & [Consul](https://www.consul.io/) 使用起来配置都十分类似，不过需要外部启动对应的服务。
 payment8004是一个使用zookeeper的demo，可以看到对应的配置。
 - Eureka主要考虑AP
 - Zookeeper和Consul主要考虑CP
+
+[Ribbon](https://github.com/Netflix/ribbon):
+是什么？
+- 负载均衡的工具。主要功能是提供客户端的负载均衡算法和服务调用
+- 在配置文件中列出LB后面所有的Instance，可以帮助基于某种规则(如简单轮询，随机连接等)去连接这些Instance
+- 可以根据Ribbon实现自定义负载均衡算法
+Eureka Client自带Ribbon
+
+工作步骤:
+1. 选择Eureka Server，查询可用的服务列表；
+2. 根据用户指定的策略，从取到的服务列表中选择一个地址；
+
+核心组件IRule:  
+根据特定的算法从服务列表中选取一个要访问的服务
--- cloud-consumer-order9999/src/main/java/com/hruan/springcloud/controller/OrderController.java
+++ cloud-consumer-order9999/src/main/java/com/hruan/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.hruan.springcloud.controller;
 import com.hruan.springcloud.entities.CommonResult;
 import com.hruan.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +28,11 @@ public class OrderController {
     @GetMapping("/consumers/payments/{id}")
     public CommonResult getPayment(@PathVariable("id") Long id) {
         log.info("**** Get Payment by id: " + id);
-        return restTemplate.getForObject(PAYMENT_URL + "/payments/" + id, CommonResult.class );
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payments/" + id, CommonResult.class);
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            return entity.getBody();
+        } else {
+            return new CommonResult(400, "Fail to get Payment");
+        }
     }
 }

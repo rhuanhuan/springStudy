30be4c5a1e0df0d45206e60fa41f5fc5feb3b647
rhuanhuan <1210098674@qq.com>
2020-07-06 09:09:48 +0800

    添加cloud config server配置

---
+++ cloud-config-center3344/src/main/java/com/hruan/springcloud/ConfigServer3344.java
@@ -0,0 +1,13 @@
+package com.hruan.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServer3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServer3344.class);
+    }
+}
